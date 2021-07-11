66c8d47fe7960b8982eaa02041a8edc18ee612df
Ubuntu20 <vishnuc77@gmail.com>
2021-07-11 11:18:10 -0700

    Edited test file

--- test/testItemManager.js
+++ test/testItemManager.js
@@ -10,6 +10,7 @@ contract("ItemManager", accounts => {
 
         const result = await itemManagerInstance.createItem(itemName, itemPrice, { from: accounts[0] });
         //assert.equal(result.logs[0].args._itemIndex, 0, "There should be only one item in the list");
+        console.log(result);
         const item = await itemManagerInstance.items(0);
         //assert.equal(item._identifier, itemName, "The item has a different identifier");
         console.log(item);
